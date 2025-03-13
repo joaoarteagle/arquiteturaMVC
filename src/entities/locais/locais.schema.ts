e4705cee364d389776b1a4e748ab5b848e4340ce
JoaoAndrade <joaopaulo.arteagle17@gmail.com>
2025-03-13 20:03:03 -0300

    front requisitando api na index para apresentar todos os locais

--- src/entities/locais/locais.schema.ts
+++ src/entities/locais/locais.schema.ts
@@ -4,5 +4,6 @@ import * as mongoose from 'mongoose';
 export const LocaisSchema = new mongoose.Schema({
     id: String,
     nome: String,
-    imagem: String,
+    local: String,
+    imagens: [String],
 });
